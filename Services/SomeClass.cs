db559af67518bd910eae83bbf4c1c414e203c4ad
jonasdamsbo <56339080+jonasdamsbo@users.noreply.github.com>
2024-04-02 18:08:51 +0200

    appsettings and debug

---
+++ Services/SomeClass.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace myblazor.Services
+{
+	public class SomeClass
+    {
+        private IConfiguration _configuration;
+
+        public SomeClass(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string SomeMethod()
+        {
+            // retrieve nested App Service app setting
+            var myHierarchicalConfig = _configuration["My:Hierarchical:Config:Data"];
+            // retrieve App Service connection string
+            var myConnString = _configuration.GetConnectionString("DefaultConnection");
+            Debug.WriteLine(myConnString);
+            return myConnString;
+        }
+    }
+}
