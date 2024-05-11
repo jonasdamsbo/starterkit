2502901f167ba642ef4a116de1ad4d6ab7030196
jonasdamsbo <56339080+jonasdamsbo@users.noreply.github.com>
2024-05-11 02:19:59 +0200

    Update Program.cs

--- myapi/Program.cs
+++ myapi/Program.cs
@@ -28,6 +28,15 @@ builder.Services.Configure<IISOptions>(options =>
 
 var app = builder.Build();
 
+// update-database on build
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<DataContext>();
+    context.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
