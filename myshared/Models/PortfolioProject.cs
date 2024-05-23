c4a941e8e6e30dc70860b6f99abafbeda818226f
jonasdamsbo <56339080+jonasdamsbo@users.noreply.github.com>
2024-05-23 16:00:45 +0200

    update

--- myshared/Models/PortfolioProject.cs
+++ myshared/Models/PortfolioProject.cs
@@ -6,5 +6,6 @@
         public string? Title { get; set; }
         public string? Description { get; set; }
         public string? WebUrl { get; set; }
-    }
+		public string? Secret { get; set; }
+	}
 }
