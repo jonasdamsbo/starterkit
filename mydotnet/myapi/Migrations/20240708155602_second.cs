49cb476b0b834f6ce8edb13c31231caf335ccef3
jonasdamsbo <56339080+jonasdamsbo@users.noreply.github.com>
2024-07-08 17:56:16 +0200

    new migration

---
+++ mydotnet/myapi/Migrations/20240708155602_second.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace myapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Secret",
+                table: "PortfolioProjects",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "PortfolioProjects",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Secret",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "PortfolioProjects",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Secret",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "PortfolioProjects",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Secret",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Secret",
+                table: "PortfolioProjects");
+        }
+    }
+}
