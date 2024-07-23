435ec8c7f44143d81203c28664894d57dec8e2a0
jonasdamsbo <56339080+jonasdamsbo@users.noreply.github.com>
2024-07-23 03:45:12 +0200

    Update PortfolioProject.cs

--- mydotnet/myshared/Models/PortfolioProject.cs
+++ mydotnet/myshared/Models/PortfolioProject.cs
@@ -8,6 +8,7 @@ namespace myshared.Models
         public string? Title { get; set; }
         public string? Description { get; set; }
         public string? WebUrl { get; set; }
+        public string? Secret { get; set; }
 
 		public PortfolioProject() { }
 		public PortfolioProject(PortfolioProjectDTO portfolioProjectDTO) =>
