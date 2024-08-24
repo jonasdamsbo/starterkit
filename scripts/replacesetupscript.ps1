089de399c7f17a8512cd754fd0d7ce4de0b614df
jonasdamsbo <56339080+jonasdamsbo@users.noreply.github.com>
2024-08-24 04:16:39 +0200

    can create pipeline from script

--- scripts/replacesetupscript.ps1
+++ scripts/replacesetupscript.ps1
@@ -1,3 +1,9 @@
-# replace setup script with new created repo path
+# replace setup script in newRepoName folder 
 
-# push everything to new repo
+# push everything to newRepoName repo
+
+#create new azure pipeline
+write-host "Trying to create pipeline in azure"
+#$ymlPath = $PWD.Path + '\azurepipeline.yml'
+az pipelines create --name "testpipeline" --yml-path '\scripts\azurepipeline.yml' --detect true
+Read-Host "Press enter to continue..."
