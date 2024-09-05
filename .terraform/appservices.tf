58b0706ad919b6435bcb3a6a006564889cba4e6d
jonasdamsbo <56339080+jonasdamsbo@users.noreply.github.com>
2024-09-05 10:59:49 +0200

    Update appservices.tf

--- .terraform/appservices.tf
+++ .terraform/appservices.tf
@@ -6,15 +6,17 @@ resource "azurerm_service_plan" "exampleAppserviceplan" {
   sku_name            = "F1"
 }
 
-resource "azurerm_app_service" "exampleWebapp" {
+resource "azurerm_windows_web_app" "exampleWebapp" {
   name                = "tempresourcenamewebapp"
   location            = data.azurerm_resource_group.exampleResourcegroup.location
   resource_group_name = data.azurerm_resource_group.exampleResourcegroup.name
-  app_service_plan_id = azurerm_service_plan.exampleAppserviceplan.id
+  service_plan_id = azurerm_service_plan.exampleAppserviceplan.id
+  public_network_access_enabled = true
 
   site_config {
-    dotnet_framework_version = "v4.0"
+    #dotnet_framework_version = "v4.0"
     scm_type                 = "LocalGit"
+    ip_restriction_default_action = "Allow"
   }
 
   app_settings = {
