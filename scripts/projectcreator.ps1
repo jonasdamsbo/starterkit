d2547a8d8fc15314267803e98671c70610a3594e
jonasdamsbo <jonasdamsbo@hotmail.com>
2024-09-13 19:07:52 +0200

    Update projectcreator.ps1

--- scripts/projectcreator.ps1
+++ scripts/projectcreator.ps1
@@ -383,7 +383,7 @@ if($verifySetup -eq "y")
         write-host "Getting and replacing tenantid, clientid, clientsecret..."
         $subscriptionId = $subscriptionId
 
-        $applicationName = $resourcename"application"
+        $applicationName = $resourcename"enterpriseapplication"
         $appDetails = az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/$subscriptionid" --name $applicationName
         write-host "appdetails: $appDetails"
 
