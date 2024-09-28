516e6461bfb0498027879b9d31ecd1d5f5756d10
jonasdamsbo <jonasdamsbo@hotmail.com>
2024-09-28 17:16:34 +0200

    Update ExampleDTO.cs

--- mydotnet/myshared/DTOs/ExampleDTO.cs
+++ mydotnet/myshared/DTOs/ExampleDTO.cs
@@ -11,9 +11,9 @@ namespace myshared.DTOs
 		public ExampleDTO() { }
 		public ExampleDTO(ExampleModel example) =>
 		(Id, Title, Description) = (example.Id, example.Title, example.Description);
-		public ExampleDTO(ExampleMssqlModel example) =>
-		(Id, Title, Description) = (example.Id, example.Title, example.Description);
-		public ExampleDTO(ExampleNosqlModel example) =>
-		(Id, Title, Description) = (example.Id, example.Title, example.Description);
+		//public ExampleDTO(ExampleMssqlModel example) =>
+		//(Id, Title, Description) = (example.Id, example.Title, example.Description);
+		//public ExampleDTO(ExampleNosqlModel example) =>
+		//(Id, Title, Description) = (example.Id, example.Title, example.Description);
 	}
 }
