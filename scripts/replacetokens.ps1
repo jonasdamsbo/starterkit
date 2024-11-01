8fe4ad5076d554cfa1eed8041bd344b87697bfd6
jonasdamsbo <jonasdamsbo@hotmail.com>
2024-11-01 17:46:09 +0100

    Update replacetokens.ps1

--- scripts/replacetokens.ps1
+++ scripts/replacetokens.ps1
@@ -60,13 +60,37 @@ Write-Host "TOKENS ARE BEING REPLACED"
 
 # check files before
     write-host "printing content of main.tf"
-    write-host "path: "+$PWD.Path
-    cd ..
-    write-host "path: "+$PWD.Path
-    cd .terraform
-    write-host "path: "+$PWD.Path
+    $myrootpath = $PWD.Path
+    $terraformpath = Get-ChildItem -Path $myrootpath -Filter 'main.tf' -Recurse -ErrorAction SilentlyContinue |
+                     Select-Object -Expand Directory -Unique |
+                     Select-Object -Expand FullName
 
-    write-host (Get-Content -path main.tf -Raw)
+    $scriptspath = Get-ChildItem -Path $myrootpath -Filter 'replacetokens.ps1' -Recurse -ErrorAction SilentlyContinue |
+    Select-Object -Expand Directory -Unique |
+    Select-Object -Expand FullName
+
+    $maintfpath = $terraformpath+"/main.tf"
+    $appservicestfpath = $terraformpath+"/appservices.tf"
+    $nosqldatabasestfpath = $terraformpath+"/nosqldatabases.tf"
+    $sqldatabasestfpath = $terraformpath+"/sqldatabases.tf"
+    $setcloudvarsps1path = $scriptspath+"/setcloudvars.tf"
+
+    write-host "path: "$myrootpath
+    write-host "path: "$terraformpath
+    write-host "path: "$scriptspath
+    write-host "path: "$maintfpath
+    write-host "path: "$appservicestfpath
+    write-host "path: "$nosqldatabasestfpath
+    write-host "path: "$sqldatabasestfpath
+    write-host "path: "$setcloudvarsps1path
+
+    # write-host "path: "$myrootpath
+    # cd ..
+    # write-host "path: "$myrootpath
+    # cd .terraform
+    # write-host "path: "$myrootpath
+
+    write-host (Get-Content -path $maintfpath -Raw)
     write-host "done printing content of main.tf"
 
     # write-host "printing content of appservices.tf"
@@ -89,88 +113,88 @@ write-host "started replacing"
 
 # replace terraform .tf tokens
     #replace values
-    ((Get-Content -path main.tf -Raw) -replace 'tempclientid',$clientid) | Set-Content -Path main.tf
-    ((Get-Content -path main.tf -Raw) -replace 'tempclientsecret',$clientsecret) | Set-Content -Path main.tf
-    ((Get-Content -path main.tf -Raw) -replace 'temptenantid',$tenantid) | Set-Content -Path main.tf
-    ((Get-Content -path main.tf -Raw) -replace 'temporganizationname',$organizationname) | Set-Content -Path main.tf
-    ((Get-Content -path main.tf -Raw) -replace 'tempsubscriptionid',$subscriptionid) | Set-Content -Path main.tf
-    ((Get-Content -path main.tf -Raw) -replace 'tempprojectname',$projectname) | Set-Content -Path main.tf
-    ((Get-Content -path main.tf -Raw) -replace 'tempresourcename',$resourcename) | Set-Content -Path main.tf
-    ((Get-Content -path main.tf -Raw) -replace 'tempazuredevopsprojectid',$projectid) | Set-Content -Path main.tf
-    ((Get-Content -path main.tf -Raw) -replace 'tempazurerepositoryid',$repositoryid) | Set-Content -Path main.tf
-    ((Get-Content -path main.tf -Raw) -replace 'temppipelineid',$pipelineid) | Set-Content -Path main.tf
-    ((Get-Content -path main.tf -Raw) -replace 'tempresourcegroupid',$resourcegroupid) | Set-Content -Path main.tf
-    ((Get-Content -path main.tf -Raw) -replace 'tempstorageaccountid',$storageaccountid) | Set-Content -Path main.tf
-    ((Get-Content -path main.tf -Raw) -replace 'tempstoragekey',$storagekey) | Set-Content -Path main.tf
-    ((Get-Content -path main.tf -Raw) -replace 'tempterraformcontainername',$storageaccountid) | Set-Content -Path main.tf
+    ((Get-Content -path $maintfpath -Raw) -replace 'tempclientid',$clientid) | Set-Content -Path $maintfpath
+    ((Get-Content -path $maintfpath -Raw) -replace 'tempclientsecret',$clientsecret) | Set-Content -Path $maintfpath
+    ((Get-Content -path $maintfpath -Raw) -replace 'temptenantid',$tenantid) | Set-Content -Path $maintfpath
+    ((Get-Content -path $maintfpath -Raw) -replace 'temporganizationname',$organizationname) | Set-Content -Path $maintfpath
+    ((Get-Content -path $maintfpath -Raw) -replace 'tempsubscriptionid',$subscriptionid) | Set-Content -Path $maintfpath
+    ((Get-Content -path $maintfpath -Raw) -replace 'tempprojectname',$projectname) | Set-Content -Path $maintfpath
+    ((Get-Content -path $maintfpath -Raw) -replace 'tempresourcename',$resourcename) | Set-Content -Path $maintfpath
+    ((Get-Content -path $maintfpath -Raw) -replace 'tempazuredevopsprojectid',$projectid) | Set-Content -Path $maintfpath
+    ((Get-Content -path $maintfpath -Raw) -replace 'tempazurerepositoryid',$repositoryid) | Set-Content -Path $maintfpath
+    ((Get-Content -path $maintfpath -Raw) -replace 'temppipelineid',$pipelineid) | Set-Content -Path $maintfpath
+    ((Get-Content -path $maintfpath -Raw) -replace 'tempresourcegroupid',$resourcegroupid) | Set-Content -Path $maintfpath
+    ((Get-Content -path $maintfpath -Raw) -replace 'tempstorageaccountid',$storageaccountid) | Set-Content -Path $maintfpath
+    ((Get-Content -path $maintfpath -Raw) -replace 'tempstoragekey',$storagekey) | Set-Content -Path $maintfpath
+    ((Get-Content -path $maintfpath -Raw) -replace 'tempterraformcontainername',$storageaccountid) | Set-Content -Path $maintfpath
 
-    ((Get-Content -path appservices.tf -Raw) -replace 'tempprojectname',$projectname) | Set-Content -Path appservices.tf
-    ((Get-Content -path appservices.tf -Raw) -replace 'tempresourcename',$resourcename) | Set-Content -Path appservices.tf
-    ((Get-Content -path appservices.tf -Raw) -replace 'tempapiurl',$apiurl) | Set-Content -Path appservices.tf
-    ((Get-Content -path appservices.tf -Raw) -replace 'tempsqlconnectionstring',$mssqlconnectionstring) | Set-Content -Path appservices.tf
-    ((Get-Content -path appservices.tf -Raw) -replace 'tempnosqlconnectionstring',$nosqlconnectionstring) | Set-Content -Path appservices.tf
+    ((Get-Content -path $appservicestfpath -Raw) -replace 'tempprojectname',$projectname) | Set-Content -Path $appservicestfpath
+    ((Get-Content -path $appservicestfpath -Raw) -replace 'tempresourcename',$resourcename) | Set-Content -Path $appservicestfpath
+    ((Get-Content -path $appservicestfpath -Raw) -replace 'tempapiurl',$apiurl) | Set-Content -Path $appservicestfpath
+    ((Get-Content -path $appservicestfpath -Raw) -replace 'tempsqlconnectionstring',$mssqlconnectionstring) | Set-Content -Path $appservicestfpath
+    ((Get-Content -path $appservicestfpath -Raw) -replace 'tempnosqlconnectionstring',$nosqlconnectionstring) | Set-Content -Path $appservicestfpath
 
-    ((Get-Content -path sqldatabases.tf -Raw) -replace 'tempprojectname',$projectname) | Set-Content -Path sqldatabases.tf
-    ((Get-Content -path sqldatabases.tf -Raw) -replace 'tempresourcename',$resourcename) | Set-Content -Path sqldatabases.tf
+    ((Get-Content -path $sqldatabasestfpath -Raw) -replace 'tempprojectname',$projectname) | Set-Content -Path $sqldatabasestfpath
+    ((Get-Content -path $sqldatabasestfpath -Raw) -replace 'tempresourcename',$resourcename) | Set-Content -Path $sqldatabasestfpath
 
-    ((Get-Content -path nosqldatabases.tf -Raw) -replace 'tempprojectname',$projectname) | Set-Content -Path nosqldatabases.tf
-    ((Get-Content -path nosqldatabases.tf -Raw) -replace 'tempresourcename',$resourcename) | Set-Content -Path nosqldatabases.tf
+    ((Get-Content -path $nosqldatabasestfpath -Raw) -replace 'tempprojectname',$projectname) | Set-Content -Path $nosqldatabasestfpath
+    ((Get-Content -path $nosqldatabasestfpath -Raw) -replace 'tempresourcename',$resourcename) | Set-Content -Path $nosqldatabasestfpath
 
 # replace setcloudvars.ps1 tokens
     #replace values
     #((Get-Content -path setcloudvars.ps1 -Raw) -replace 'tempresourcename',$resourcename) | Set-Content -Path setcloudvars.ps1
 
-    cd ..
-    write-host "path: "+$PWD.Path
-    cd scripts
-    write-host "path: "+$PWD.Path
+    # cd ..
+    # write-host "path: "$myrootpath
+    # cd scripts
+    # write-host "path: "$myrootpath
 
-    ((Get-Content -path setcloudvars.ps1 -Raw) -replace 'tempapiurl',$apiurl) | Set-Content -Path setcloudvars.ps1
-    ((Get-Content -path setcloudvars.ps1 -Raw) -replace 'tempnosqlconnectionstring',$nosqlconnectionstring) | Set-Content -Path setcloudvars.ps1
-    ((Get-Content -path setcloudvars.ps1 -Raw) -replace 'tempmssqlconnectionstring',$mssqlconnectionstring) | Set-Content -Path setcloudvars.ps1
-    ((Get-Content -path setcloudvars.ps1 -Raw) -replace 'tempresourcegroupname',$resourcegroupname) | Set-Content -Path setcloudvars.ps1
-    ((Get-Content -path setcloudvars.ps1 -Raw) -replace 'tempwebappname',$webappname) | Set-Content -Path setcloudvars.ps1
-    ((Get-Content -path setcloudvars.ps1 -Raw) -replace 'tempapiappname',$apiappname) | Set-Content -Path setcloudvars.ps1
-    ((Get-Content -path setcloudvars.ps1 -Raw) -replace 'tempmssqlservername',$mssqlservername) | Set-Content -Path setcloudvars.ps1
-    ((Get-Content -path setcloudvars.ps1 -Raw) -replace 'tempmssqldatabasename',$mssqldatabasename) | Set-Content -Path setcloudvars.ps1
-    ((Get-Content -path setcloudvars.ps1 -Raw) -replace 'tempstorageaccountname',$storageaccountname) | Set-Content -Path setcloudvars.ps1
-    ((Get-Content -path setcloudvars.ps1 -Raw) -replace 'tempstoragekey',$storagekey) | Set-Content -Path setcloudvars.ps1
-    ((Get-Content -path setcloudvars.ps1 -Raw) -replace 'tempdbbackupcontainername',$dbbackupcontainername) | Set-Content -Path setcloudvars.ps1
-    ((Get-Content -path setcloudvars.ps1 -Raw) -replace 'tempcosmosdbaccountname',$nosqlaccountname) | Set-Content -Path setcloudvars.ps1
-    ((Get-Content -path setcloudvars.ps1 -Raw) -replace 'tempcosmosmongodbname',$nosqldatabasename) | Set-Content -Path setcloudvars.ps1
+    ((Get-Content -path $setcloudvarsps1path -Raw) -replace 'tempapiurl',$apiurl) | Set-Content -Path $setcloudvarsps1path
+    ((Get-Content -path $setcloudvarsps1path -Raw) -replace 'tempnosqlconnectionstring',$nosqlconnectionstring) | Set-Content -Path $setcloudvarsps1path
+    ((Get-Content -path $setcloudvarsps1path -Raw) -replace 'tempmssqlconnectionstring',$mssqlconnectionstring) | Set-Content -Path $setcloudvarsps1path
+    ((Get-Content -path $setcloudvarsps1path -Raw) -replace 'tempresourcegroupname',$resourcegroupname) | Set-Content -Path $setcloudvarsps1path
+    ((Get-Content -path $setcloudvarsps1path -Raw) -replace 'tempwebappname',$webappname) | Set-Content -Path $setcloudvarsps1path
+    ((Get-Content -path $setcloudvarsps1path -Raw) -replace 'tempapiappname',$apiappname) | Set-Content -Path $setcloudvarsps1path
+    ((Get-Content -path $setcloudvarsps1path -Raw) -replace 'tempmssqlservername',$mssqlservername) | Set-Content -Path $setcloudvarsps1path
+    ((Get-Content -path $setcloudvarsps1path -Raw) -replace 'tempmssqldatabasename',$mssqldatabasename) | Set-Content -Path $setcloudvarsps1path
+    ((Get-Content -path $setcloudvarsps1path -Raw) -replace 'tempstorageaccountname',$storageaccountname) | Set-Content -Path $setcloudvarsps1path
+    ((Get-Content -path $setcloudvarsps1path -Raw) -replace 'tempstoragekey',$storagekey) | Set-Content -Path $setcloudvarsps1path
+    ((Get-Content -path $setcloudvarsps1path -Raw) -replace 'tempdbbackupcontainername',$dbbackupcontainername) | Set-Content -Path $setcloudvarsps1path
+    ((Get-Content -path $setcloudvarsps1path -Raw) -replace 'tempcosmosdbaccountname',$nosqlaccountname) | Set-Content -Path $setcloudvarsps1path
+    ((Get-Content -path $setcloudvarsps1path -Raw) -replace 'tempcosmosmongodbname',$nosqldatabasename) | Set-Content -Path $setcloudvarsps1path
 
 write-host "done replacing"
 
 # check files after
 
-    cd ..
-    write-host "path: "+$PWD.Path
-    cd .terraform
-    write-host "path: "+$PWD.Path
+    # cd ..
+    # write-host "path: "$myrootpath
+    # cd .terraform
+    # write-host "path: "$myrootpath
 
     write-host "printing content of main.tf"
-    write-host (Get-Content -path main.tf -Raw)
+    write-host (Get-Content -path $maintfpath -Raw)
     write-host "done printing content of main.tf"
 
     write-host "printing content of appservices.tf"
-    write-host (Get-Content -path appservices.tf -Raw)
+    write-host (Get-Content -path $appservicestfpath -Raw)
     write-host "done printing content of appservices.tf"
 
     write-host "printing content of sqldatabases.tf"
-    write-host (Get-Content -path sqldatabases.tf -Raw)
+    write-host (Get-Content -path $sqldatabasestfpath -Raw)
     write-host "done printing content of sqldatabases.tf"
 
     write-host "printing content of nosqldatabases.tf"
-    write-host (Get-Content -path nosqldatabases.tf -Raw)
+    write-host (Get-Content -path $nosqldatabasestfpath -Raw)
     write-host "done printing content of nosqldatabases.tf"
 
-    cd ..
-    write-host "path: "+$PWD.Path
-    cd scripts
-    write-host "path: "+$PWD.Path
+    # cd ..
+    # write-host "path: "$myrootpath
+    # cd scripts
+    # write-host "path: "$myrootpath
 
     write-host "printing content of setcloudvars.ps1"
-    write-host (Get-Content -path setcloudvars.ps1 -Raw)
+    write-host (Get-Content -path $setcloudvarsps1path -Raw)
     write-host "done printing content of setcloudvars.ps1"
 
 
