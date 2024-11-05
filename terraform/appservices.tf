46464f54fded47950e89632290a34040f5cb5eed
jonasdamsbo <jonasdamsbo@hotmail.com>
2024-11-05 19:43:38 +0100

    Update appservices.tf

--- terraform/appservices.tf
+++ terraform/appservices.tf
@@ -21,8 +21,7 @@ resource "azurerm_windows_web_app" "exampleWebapp" {
   }
 
   app_settings = {
-    #"MyAppSettings:APIURL" = "tempapiurl"
-    "MyAppSettings:APIURL" = "https://tempresourcenameapiapp.azurewebsites.net/" #azurerm_windows_web_app.exampleApiapp.default_hostname
+    "MyAppSettings:APIURL" = "https://tempresourcenameapiapp.azurewebsites.net/" #azurerm_windows_web_app.exampleApiapp.default_hostname #"tempapiurl"
     "ASPNETCORE_ENVIRONMENT" = "Production"
   }
 }
@@ -38,22 +37,9 @@ resource "azurerm_windows_web_app" "exampleApiapp" {
     #scm_type                 = "LocalGit"
     ip_restriction_default_action = "Deny"
     always_on = "false"
-    
-    # ip_restriction {
-    #   ip_address = azurerm_windows_web_app.exampleWebapp.outbound_ip_addresses #"tempwebappip"
-    #   action = "Allow"
-    #   priority = 1
-    # }
-
-    # ip_restriction {
-    #   ip_address = "templocalip"
-    #   action = "Allow"
-    #   priority = 1
-    # }
   }
 
   app_settings = {
-    #"NosqlDatabase:ConnectionString" = "tempnosqlconnectionstring"
     "NosqlDatabase:ConnectionString" = "mongodb+srv://sa:'tempnosqlpassword'@tempresourcenamecosmosmongodb.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000"
     "NosqlDatabase:DatabaseName" = "tempresourcenamecosmosmongodb"
 
@@ -68,12 +54,5 @@ resource "azurerm_windows_web_app" "exampleApiapp" {
     name  = "Mssql"
     type  = "SQLServer"
     value = "Server=tcp:tempresourcenamemssqlserver.database.windows.net,1433;Initial Catalog=tempresourcenamemssqldatabase;Persist Security Info=False;User ID=tempresourcename;Password=tempsqlpassword;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-    #value = "tempsqlconnectionstring"
   }
-
-  # connection_string {
-  #   name  = "Nosql"
-  #   type  = "DocDb"
-  #   value = "tempnosqlconnectionstring"
-  # }
 }
